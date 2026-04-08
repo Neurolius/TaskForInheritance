2bd31ce1e3d2b88b5e4beb1804efb90559f65c55
neurolius <ewgensafonow@gmail.com>
2026-04-08 12:27:25 +0800

    Типы и виды продуктов из строк заменены на Enum

--- Inheritance/Drinks.cs
+++ Inheritance/Drinks.cs
@@ -1,7 +1,4 @@
-﻿
-
-
-namespace Inheritance
+﻿namespace Inheritance
 {
     public class Drink {
         public double Volume = 0;
@@ -12,59 +9,80 @@ namespace Inheritance
         public static Random rnd = new Random();
     }
 
+    public enum Fruit
+    {
+        Apple,
+        Orange,
+        Pear,
+        Grape
+    }
+
     public class Juice : Drink {
-        public string UsingFruit = "Яблоко";
+        public Fruit usingFruit = Fruit.Apple;
         public bool hasPulp = true;
 
         public override string getInfo()
         {
             var str = "Я сок";
             str += base.getInfo();
-            str += String.Format("\nИспользуемый фрукт: {0}", this.UsingFruit);
+            str += String.Format("\nИспользуемый фрукт: {0}", this.usingFruit);
             str += String.Format("\nНаличие мякоти: {0}", this.hasPulp);
             return str;
         }
 
         public static Juice Generate()
         {
-            var Fruits = new string[] { "Яблоко", "Апельсин", "Груша", "Виноград" };
-
             return new Juice { 
                 Volume = rnd.Next(1, 10),
-                UsingFruit = Fruits[rnd.Next(0,Fruits.Length)],
+                usingFruit = (Fruit)rnd.Next(4),
                 hasPulp = rnd.Next(0, 2) == 1
             };
         }
     }
 
+    public enum SodaType
+    {
+        Cola,
+        Fanta,
+        Sprite,
+        Pepsi
+    }
+
     public class Soda : Drink {
-        public string Type = "Добрый кола";
-        public int CountBubbles = 500;
+        public SodaType Type = SodaType.Cola;
+        public int countBubbles = 500;
 
         public override string getInfo()
         {
             var str = "Я газировка";
             str += base.getInfo();
             str += String.Format("\nВид газировки: {0}", this.Type);
-            str += String.Format("\nКоличество пузырьков: {0}", this.CountBubbles);
+            str += String.Format("\nКоличество пузырьков: {0}", this.countBubbles);
             return str;
         }
 
         public static Soda Generate()
         {
-            var Types = new string[] { "Добрый кола", "Фанта", "Спрайт", "Пепси" };
             return new Soda
             {
                 Volume = rnd.Next(1, 10),
-                Type = Types[rnd.Next(0, Types.Length)],
-                CountBubbles = rnd.Next(100, 1000)
+                Type = (SodaType)rnd.Next(4),
+                countBubbles = rnd.Next(100, 1000)
             };
         }
     }
 
+    public enum AlcoholType
+    {
+        Wine,
+        Beer,
+        Vodka,
+        Cognac
+    }
+
     public class Alcohol : Drink { 
         public int Strength = 50;
-        public string Type = "Вино";
+        public AlcoholType Type = AlcoholType.Wine;
 
         public override string getInfo()
         {
@@ -77,12 +95,11 @@ namespace Inheritance
 
         public static Alcohol Generate()
         {
-            var Types = new string[] { "Вино", "Пиво", "Водка", "Коньяк" };
             return new Alcohol
             {
                 Volume = rnd.Next(1, 10),
                 Strength = rnd.Next(1, 100),
-                Type = Types[rnd.Next(0, Types.Length)]
+                Type = (AlcoholType)rnd.Next(4),
             };
         }
     }
